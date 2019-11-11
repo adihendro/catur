dea96e195cdb6b089fad2fa9133367f8a5d915fa
adihendro <adi21hendro@gmail.com>
2019-11-11 11:31:32 +0700

    cek_semua_gerak done

--- cek_semua_gerak.c
+++ cek_semua_gerak.c
@@ -5,7 +5,7 @@
 #include "boolean.h"
 
 #define kotak board[F.posisiR + a*x1*x2][F.posisiC + b*x1*x2]
-int i,j,k,temp;
+int i,j,k,a,b,temp;
 boolean br, enemy;
 
 void cek(int a, int b, int x1, int x2, piece F, papan board[10][10]){
@@ -14,45 +14,44 @@ void cek(int a, int b, int x1, int x2, piece F, papan board[10][10]){
     if(kotak.nama==' '){ //kalau kotak kosong
         //InsVLast(L,F);
         printf("kosong\n");
-    } else{
+    } else{ //kotak = */temen/musuh
+        br=true; //break loop
         if(kotak.nama!='*' && kotak.player!=F.player){ //kalau kotak = musuh
             //InsVLast(L,F);
             printf("musuh\n");
             enemy=true;       
         }
-        br=true; //break loop
     }
 }
 
 void pawn(piece F, papan board[10][10]){
-    int a = F.player==1 ? -1 : 1;
+    a = F.player==1 ? -1 : 1;
     for(i=-1;i<=1;i++){
         cek(a,i,1,1,F,board);
     }
 }
 
 void rook(piece F, papan board[10][10]){
-    int a=1, b=0;
+    a=1, b=0;
     for(i=0;i<2;i++){
         for(j=-1;j<=1;j=j+2){
             for(k=1;k<=7;k++){
                 cek(a,b,j,k,F,board);
-                if(br){break;}
+                if(br){printf("\n");break;}
             }
         }
-        temp=a;
-        a=b;
-        b=temp;
+        a=0;
+        b=1;
     }
 }
 
 void bishop(piece F, papan board[10][10]){
-    int a=1;
+    a=1;
     for(i=0;i<2;i++){
         for(j=-1;j<=1;j=j+2){
             for(k=1;k<=7;k++){
                 cek(a,1,j,k,F,board);
-                if(br){break;}
+                if(br){printf("\n");break;}
             }
         }
         a=-1;
@@ -60,26 +59,29 @@ void bishop(piece F, papan board[10][10]){
 }
 
 void king(piece F, papan board[10][10]){
-    int a=1, b=1;
+    a=1, b=1;
     for(i=0;i<8;i++){
         cek(a,b,1,1,F,board);
         temp=a;
         a=b;
         b=-temp;
-        if(i==4){
+        if(i==3){
             a=0;
         }
     }
 }
 
 void knight(piece F, papan board[10][10]){
-    int a=1, b=2;
+    a=1, b=2;
     for(i=0;i<8;i++){
-        cek(a,b,1,1,F,board);
+        if((F.posisiR+a) >=1 && (F.posisiR+a) <=8 && 
+           (F.posisiC+b) >=1 && (F.posisiC+b) <=8){
+            cek(a,b,1,1,F,board);
+        }
         temp=a;
         a=b;
         b=-temp;
-        if(i==4){
+        if(i==3){
             a=2;
             b=1;
         }
@@ -87,21 +89,6 @@ void knight(piece F, papan board[10][10]){
 }
 
 void ceksemuagerak(piece F, papan board[10][10]){
-    // if(F.nama=='P'){
-    //     pawn(F, board);
-    // } else if(F.nama=='R'){
-    //     rook(F, board);
-    // } else if(F.nama=='B'){
-    //     bishop(F, board);
-    // } else if(F.nama=='Q'){
-    //     rook(F, board);
-    //     bishop(F, board);
-    // } else if(F.nama=='K'){
-    //     king(F, board);
-    // } else if(F.nama=='N'){
-    //     knight(F, board);
-    // }
-
     switch(F.nama){
         case 'P': pawn(F, board); break;
         case 'R': rook(F, board); break;
@@ -114,24 +101,44 @@ void ceksemuagerak(piece F, papan board[10][10]){
 
 
 
-
 int main() {
     piece F;
     papan board[10][10];
-    F.nama = 'P';
+    F.nama = 'K';
     F.poin = 1;
     F.player = 1;
-    F.posisiR = 3;
-    F.posisiC = 5;
+    F.posisiR = 2;
+    F.posisiC = 2;
 
-    board[2][4].nama = 'P';
-    board[2][4].player = 1;
+    for(i=1;i<=8;i++){
+        for(j=1;j<=8;j++){
+            board[i][j].nama = ' ';
+        }
+    }
+
+    board[1][1].nama = 'P';
+    board[1][1].player = 1;
+
+    board[1][2].nama = 'P';
+    board[1][2].player = 2;
+
+    board[1][3].nama = ' ';
+    board[1][3].player = 1;
+
+    board[2][1].nama = ' ';
+    board[2][1].player = 1;
+
+    board[2][3].nama = 'Q';
+    board[2][3].player = 1;
     
-    board[2][5].nama = ' ';
-    board[2][5].player = 1;
+    board[3][1].nama = 'P';
+    board[3][1].player = 2;
 
-    board[2][6].nama = 'P';
-    board[2][6].player = 2;
+    board[3][2].nama = ' ';
+    board[3][2].player = 2;
+
+    board[3][3].nama = ' ';
+    board[3][3].player = 1;
 
     ceksemuagerak(F,board);
     
