71b485e209e2acef2957a697780fd7ad7258bcea
adihendro <adi21hendro@gmail.com>
2019-11-12 00:10:55 +0700

    update main.c

--- main.c
+++ main.c
@@ -3,7 +3,23 @@
 #include "tipe_bentukan.h"
 
 int main(){
+    papan board[10][10];
+
     start();
+    PrintPapan(board);
+
+    printf("Masukkan command: ");
+    scanf("%s",command);
+
+    switch (command){
+    case "MOVE":
+        /* code */
+        break;
+    default:
+        //command salah
+        break;
+    }
+
 
     return 0;
 }
