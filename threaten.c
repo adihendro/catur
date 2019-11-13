13580d0046a882014025464772d376fb9fdbf67a
iann221 <vincentiannugroho@gmail.com>
2019-11-13 15:48:47 +0700

    threaten.c

--- threaten.c
+++ threaten.c
@@ -1,10 +1,23 @@
 #include "listlinier.h"
 #include "listposisi.h"
 
-boolean isthreaten(list lawan, list_posisi daftar_posisi, int x, int y){
-    address P;
+boolean isthreaten(list lawan, list_posisi daftar_posisi, int x, int y, papan board[10][10]){ //mengecek apakah suatu spot berbahaya untuk raja
+    address_list P; 
+    address_posisi R;
+    boolean cek = false;
     P = First(lawan);
-    while (P!=Nil){
-        ceksemuagerak(Info(P), board, )
+    CreateEmpty_posisi(&daftar_posisi);
+    while (P!=Nil && cek!=false){
+        ceksemuagerak(Info(P), board, daftar_posisi);
+        R = First(daftar_posisi);
+        while (R!=Nil && cek!=false){
+            if((Info(R).posisiC==x)&&(Info(R).posisiR==y)){
+                cek = true;
+            }
+            R = Next(R);
+        }
+        P=Next(P);
     }
+    return cek;
 }
+
