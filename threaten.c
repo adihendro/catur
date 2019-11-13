1c6b542c55384cf26e8cfab2841df8fc17ebba2c
iann221 <vincentiannugroho@gmail.com>
2019-11-13 16:07:45 +0700

    added adaorang.c

--- threaten.c
+++ threaten.c
@@ -1,23 +1,23 @@
 #include "listlinier.h"
 #include "listposisi.h"
 
-boolean isthreaten(list lawan, list_posisi daftar_posisi, int x, int y, papan board[10][10]){ //mengecek apakah suatu spot berbahaya untuk raja
+boolean isthreaten(list lawan, list_posisi daftar_posisi, int x, int y, papan board[10][10]){ //mengefound apakah suatu spot berbahaya untuk raja
     address_list P; 
     address_posisi R;
-    boolean cek = false;
+    boolean found = false;
     P = First(lawan);
     CreateEmpty_posisi(&daftar_posisi);
-    while (P!=Nil && cek!=false){
+    while (P!=Nil && found!=false){
         ceksemuagerak(Info(P), board, daftar_posisi);
         R = First(daftar_posisi);
-        while (R!=Nil && cek!=false){
+        while (R!=Nil && found!=false){
             if((Info(R).posisiC==x)&&(Info(R).posisiR==y)){
-                cek = true;
+                found = true;
             }
             R = Next(R);
         }
         P=Next(P);
     }
-    return cek;
+    return found;
 }
 
