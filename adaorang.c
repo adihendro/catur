a1f720329a9bfb570a5d97372ea96b483b980a3f
adihendro <adi21hendro@gmail.com>
2019-11-16 00:12:56 +0700

    Update adaorang.c

--- adaorang.c
+++ adaorang.c
@@ -1,9 +1,8 @@
 #include "boolean.h"
 #include "tipe_bentukan.h"
-#include "listlinier.h"
 
-boolean adaorang(papan board[10][10], int y, int x){
-    if (board[x][y].nama != ' '){
+boolean adaorang(list lawan, papan *board[10][10], int y, int x){
+    if ((*board[x][y]).nama != ' '){
         return true;
     } else {
         return false;
