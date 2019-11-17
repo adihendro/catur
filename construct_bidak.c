f59f9e5f2dd5e66581d1285108ccf0ada0b4f813
adihendro <adi21hendro@gmail.com>
2019-11-18 03:30:20 +0700

    Delete construct_bidak.c

--- construct_bidak.c
+++
@@ -1,9 +0,0 @@
-#include "tipe_bentukan.h"
-
-void ConstructBidak(piece bidak, char nama, int player, int poin, int posisiR, int posisiC){
-    bidak.nama = nama;
-    bidak.player = player;
-    bidak.poin = poin;
-    bidak.posisiC = posisiC;
-    bidak.posisiR = posisiR;
-}
