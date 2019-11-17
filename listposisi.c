f0634095b8c35b72704f685b4ed2ae9b0c666804
adihendro <adi21hendro@gmail.com>
2019-11-18 01:24:26 +0700

    Update listposisi.c

--- listposisi.c
+++ listposisi.c
@@ -38,6 +38,42 @@ void InsVLast_posisi (list_posisi *L, infotype_posisi X)
     P = Alokasi_posisi(X);
     if (P != Nil_list) 
     {
-      InsertLast(L,P);
+      InsertLast_posisi(L,P);
     }
 }
+
+
+/****************** PRIMITIF BERDASARKAN ALAMAT ******************/
+/*** PENAMBAHAN ELEMEN BERDASARKAN ALAMAT ***/
+void InsertFirst_posisi (list_posisi *L, address_posisi P)
+{
+    Next(P) = First(*L);
+    First(*L) = P;
+}
+/* I.S. Sembarang, P sudah dialokasi  */
+/* F.S. Menambahkan elemen ber-address_posisi P sebagai elemen pertama */
+void InsertAfter_posisi (list_posisi *L, address_posisi P, address_posisi Prec)
+{
+    Next(P) = Next(Prec);
+    Next(Prec) = P;
+}
+/* I.S. Prec pastilah elemen list dan bukan elemen terakhir, */
+/*      P sudah dialokasi  */
+/* F.S. Insert P sebagai elemen sesudah elemen beralamat Prec */
+void InsertLast_posisi (list_posisi *L, address_posisi P)
+{
+    address_posisi Last;
+    
+    /* Algoritma */
+    Last = First(*L);
+    if (First(*L) == Nil_list) {
+      InsertFirst_posisi(L,P);
+    } else {
+      while (Next(Last) != Nil_list) {
+        Last = Next(Last);
+      }
+      InsertAfter_posisi(L,P,Last);
+    }
+}
+/* I.S. Sembarang, P sudah dialokasi  */
+/* F.S. P ditambahkan sebagai elemen terakhir yang baru */
