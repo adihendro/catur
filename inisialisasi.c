8605317218b0aebc4e239a03cfd892b70c714e91
adihendro <adi21hendro@gmail.com>
2019-11-19 00:49:30 +0700

    create updatepapan

--- inisialisasi.c
+++ inisialisasi.c
@@ -12,13 +12,46 @@ void construct(char nama, int player, int poin, int posisiR, int posisiC, list *
     InsVFirst(L,bidak);
 }
 
+void updatepapan(papan *board[10][10], list list_ada_putih, list list_ada_hitam){
+    for(i=0;i<=9;i=i+9){
+        for(j=0;j<=9;j++){
+            (*board[i][j]).nama = '*';
+            k=i;
+            i=j;
+            j=k;
+            (*board[i][j]).nama = '*';
+        }
+    }
+
+    for(i=1;i<=8;i++){
+        for(j=1;j<=8;j++){
+            (*board[i][j]).nama = ' ';
+        }
+    }
+
+    address_list P;
+    P = First(list_ada_putih);
+    while(P != Nil_list) {
+        (*board[Info(P).posisiR][Info(P).posisiC]).nama = Info(P).nama;
+        (*board[Info(P).posisiR][Info(P).posisiC]).player = Info(P).player;
+        P = Next(P);
+    }
+    P = First(list_ada_hitam);
+    while(P != Nil_list) {
+        (*board[Info(P).posisiR][Info(P).posisiC]).nama = Info(P).nama;
+        (*board[Info(P).posisiR][Info(P).posisiC]).player = Info(P).player;
+        P = Next(P);
+    }
+}
+
+
 void inisialisasi(){
     CreateEmpty_stack(&history);
     CreateEmpty_stack(&termakan);
     CreateEmpty_list(&list_ada_putih);
     CreateEmpty_list(&list_ada_hitam);
     CreateEmpty_queue(&giliran, 2);
-    Add (&giliran, 1); //giliran pertama putih (1)
+    Add(&giliran, 1); //giliran pertama putih (1)
     poin_putih=0;
     poin_hitam=0;
 
@@ -42,5 +75,6 @@ void inisialisasi(){
     construct('P', 2, 1, 2, 8, &list_ada_hitam); //pion hitam
 
     construct('Q', 2, 8, 1, 4, &list_ada_hitam); //ratu hitam
+
 
 }
