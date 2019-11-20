09eb506b1f1a1a9e619a02156daf99ee7cc77cc0
Adi <adi@mike-pc.local>
2019-11-20 11:48:35 +0700

    commit

--- ada_orang.c
+++ ada_orang.c
@@ -1,10 +1,6 @@
 #include "boolean.h"
 #include "tipe_bentukan.h"
 
-boolean adaorang(list lawan, papan *board[10][10], int y, int x){
-    if ((*board[x][y]).nama != ' '){
-        return true;
-    } else {
-        return false;
-    }
+boolean adaorang(papan *board[10][10], int y, int x){
+    return((*board[x][y]).nama != ' ');
 }
