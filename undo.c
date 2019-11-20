a87f39a552a3feb12ed588a552ea74f83bec5b77
Adi <adi@mike-pc.local>
2019-11-20 14:10:07 +0700

    commit

---
+++ undo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "tipe_bentukan.h"
+
+void undo(stack *history, stack *termakan, int *poin_putih, int *poin_hitam, list *list_ada_putih, list *list_ada_hitam, queue *giliran){
+    
+
+    printf("Gerakan sampai giliran sebelumnya berhasil dibatalkan.\n");
+}
