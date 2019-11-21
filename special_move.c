ba7c888e3bad0c07ceffee1c6a9e849f53cc83ff
Adi <adi@mike-pc.local>
2019-11-21 18:47:42 +0700

    Update special_move.c

--- special_move.c
+++ special_move.c
@@ -3,42 +3,37 @@
 #include "stack.h"
 
 
-
-void enPassant(piece P, stack* history)
+void enpassant(piece P, stack* history)
 {
   //  if (TOP(history) = );
 
 }
-/*
+
 
 boolean HasMoved(piece P, stack history)
 {
+    infotype_stack X;
     int counter = 1;
     boolean Moved = false;
 
-    infotype_stack X;
-    
-    while ((!Moved) && (counter < (history.T[(history).TOP].turn))){   
+    while ((!Moved) && (counter <= (history.T[(history).TOP].turn))){   
         X = (history).T[(history).TOP];
         history.TOP--;
     
-        if (X.nama == P.nama){
+        if (X.nama == P.nama)
             Moved = true;
-        }
-
-       counter++;
+        counter++;
     }
-
     return Moved;
 }
 
-void Castling(piece Raja, piece Benteng , stack history, papan* board[10][10])
+void castling(piece Raja, piece Benteng, stack history, papan* board[10][10])
 {
-    if (!(HasMoved(Raja,history)))
+    if (!(HasMoved(Raja, history))) //kalau raja belum pernah gerak
     {
         if (((*board[Raja.posisiR][Raja.posisiC+2]).nama == ' ') && ((*board[Raja.posisiR][Raja.posisiC+1]).nama == " ") && (!(HasMoved(Benteng, history))))
         {
-            (*board[Raja.posisiR][Raja.posisiC+2]).nama == ' ';
+            // (*board[Raja.posisiR][Raja.posisiC+2]).nama == 'R';
         }
     }
-*/
+}
